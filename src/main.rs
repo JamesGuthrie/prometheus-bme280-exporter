ead974d0f9c7ec7cab5dc7613fffd8f474af4553
James Guthrie <james@prodigi.ch>
2022-08-25 01:56:42 +0200

    Better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,33 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use bme280::i2c::BME280;
 use hyper::server::conn::Http;
 use hyper::service::Service;
 use hyper::{Body, Method, Request, Response, StatusCode};
 use lazy_static::lazy_static;
-use linux_embedded_hal::{Delay, I2cdev};
-use prometheus::{Encoder, Gauge, TextEncoder, register_gauge};
+use linux_embedded_hal::{Delay, I2CError, I2cdev};
+use prometheus::{register_gauge, Encoder, Gauge, TextEncoder};
 use tokio::net::TcpListener;
 
-use std::convert::Infallible;
+use bme280::Measurements;
 use std::future::Future;
 use std::net::SocketAddr;
 use std::pin::Pin;
 use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll};
 
-
 lazy_static! {
-    static ref TEMPERATURE_GAUGE: Gauge =
-        register_gauge!("meter_temperature_celcius", "Ambient temperature in Celcius").unwrap();
-}
-
-lazy_static! {
+    static ref TEMPERATURE_GAUGE: Gauge = register_gauge!(
+        "meter_temperature_celsius",
+        "Ambient temperature in Celsius"
+    )
+    .unwrap();
     static ref PRESSURE_GAUGE: Gauge =
         register_gauge!("meter_pressure_pascals", "Atmospheric pressure in Pascals").unwrap();
-}
-
-lazy_static! {
     static ref HUMIDITY_GAUGE: Gauge =
         register_gauge!("meter_humidity_percent", "Relative humidity in %").unwrap();
 }
 
+const DEFAULT_DEV_PATH: &str = "/dev/i2c-1";
 
 #[derive(Clone)]
 struct TempServer {
@@ -39,24 +36,34 @@ struct TempServer {
 
 impl TempServer {
     fn new() -> Result<TempServer> {
-        // using Linux I2C Bus #1 in this example
-        let i2c_bus = I2cdev::new("/dev/i2c-1")?;
-        // initialize the BME280 using the primary I2C address 0x76
+        let i2c_bus = I2cdev::new(DEFAULT_DEV_PATH)?;
         let mut bme280 = BME280::new_primary(i2c_bus);
 
         let mut delay = Delay;
 
-        // initialize the sensor
-        bme280.init(&mut delay).unwrap();
+        bme280
+            .init(&mut delay)
+            .map_err(|e| anyhow!("unable to init: {:?}", e))?;
         Ok(TempServer {
             bme280: Arc::new(Mutex::new(bme280)),
         })
+    }
+
+    fn measure(&self) -> Result<Measurements<I2CError>> {
+        let mut delay = Delay;
+        let measurement = self
+            .bme280
+            .lock()
+            .map_err(|e| anyhow!("lock poisined: {:?}", e))?
+            .measure(&mut delay)
+            .map_err(|e| anyhow!("unable to measure: {:?}", e))?;
+        Ok(measurement)
     }
 }
 
 impl Service<Request<Body>> for TempServer {
     type Response = Response<Body>;
-    type Error = Infallible;
+    type Error = anyhow::Error;
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
     fn poll_ready(&mut self, _: &mut Context) -> Poll<Result<(), Self::Error>> {
@@ -66,9 +73,12 @@ impl Service<Request<Body>> for TempServer {
     fn call(&mut self, req: Request<Body>) -> Self::Future {
         match (req.method(), req.uri().path()) {
             (&Method::GET, "/metrics") => {
-                let mut delay = Delay;
-                // measure temperature, pressure, and humidity
-                let measurements = self.bme280.lock().unwrap().measure(&mut delay).unwrap();
+                let measurements = self.measure();
+
+                if measurements.is_err() {
+                    return Box::pin(async { Err(anyhow!("unable to measure")) });
+                }
+                let measurements = measurements.unwrap();
 
                 TEMPERATURE_GAUGE.set(measurements.temperature.into());
                 PRESSURE_GAUGE.set(measurements.pressure.into());
@@ -77,14 +87,14 @@ impl Service<Request<Body>> for TempServer {
                 let mut buffer = Vec::new();
                 let encoder = TextEncoder::new();
 
-                // Gather the metrics.
                 let metric_families = prometheus::gather();
-                // Encode them to send.
-                encoder.encode(&metric_families, &mut buffer).unwrap();
+                encoder
+                    .encode(&metric_families, &mut buffer)
+                    .expect("encoding failed");
 
-                let output = String::from_utf8(buffer.clone()).unwrap();
+                let buffer = buffer.clone();
 
-                Box::pin(async { Ok(Response::builder().body(Body::from(output)).unwrap()) })
+                Box::pin(async { Ok(Response::builder().body(Body::from(buffer)).unwrap()) })
             }
             _ => Box::pin(async {
                 Ok(Response::builder()
@@ -100,7 +110,7 @@ impl Service<Request<Body>> for TempServer {
 async fn main() -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 3002));
 
-    let server = TempServer::new().unwrap();
+    let server = TempServer::new()?;
 
     let listener = TcpListener::bind(addr).await?;
     println!("Listening on http://{}", addr);
